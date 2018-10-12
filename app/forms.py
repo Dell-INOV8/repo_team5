a350dc519133e4cc0f2a02408bf37584d12e5a9f
kayvonkhosrowpour <kayvonk@utexas.edu>
2018-10-12 12:35:54 -0500

    New posts

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
+	TextAreaField
+from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign in')
+
+class ExploreForm(FlaskForm):
+	submit_new = SubmitField('Submit New Post')
+
+class ProjectPostForm(FlaskForm):
+	title = StringField('Title', validators=[DataRequired(),
+		Length(min=10, max=100)])
+	in_progress = BooleanField('Is the project still in progress?')
+	description = TextAreaField('Description', validators=[
+        DataRequired(), Length(min=10, max=300)])
+	need_help = TextAreaField('Need Help')
+	challenges = TextAreaField('Challenges')
+	learned = TextAreaField('What I Learned')
+	submit = SubmitField('Submit New Post')
+
+	
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
-
-
+from app.forms import LoginForm, ProjectPostForm, ExploreForm
+from flask_login import current_user, LoginManager
 
 @app.route('/')
 @app.route('/index')
@@ -28,3 +27,51 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/postnew', methods=['GET', 'POST'])
+def postnew():
+    form = ProjectPostForm()
+    if form.validate():
+        # post = Post(
+        #     author=current_user,
+        #     title=form.post.title,
+        #     description=form.post.description,
+        #     learned=form.post.learned,
+        #     in_progress=form.post.in_progress,
+        #     need_help=form.post.need_help,
+        #     timestamp=form.post.timestamp
+        # )
+        # db.session.add(post)
+        # db.session.commit()
+        print('Author=', current_user)
+        print('title=', form.post.title)
+        print('etc...')
+        return redirect(url_for('explore'))
+    return render_template('postnew.html', title='Submit New Post',
+        form=form)
+
+@app.route('/explore', methods=['GET', 'POST'])
+def explore():
+    fakeposts = [
+            {
+                'title': 'Neural Network for Recognizing Handwritten Nums',
+                'description': 'This project was a lot of fun!',
+                'challenges': 'This was a really challenging project',
+                'learned': 'We learned a LOT!',
+                'in_progress': False,
+                'need_help': None,
+                'timestamp': '10-12-18 5:00pm'
+            },
+            {
+                'title': 'Some Other Project',
+                'description': 'This project was not so fun!',
+                'challenges': None,
+                'learned': None,
+                'in_progress': True,
+                'need_help': 'We need help in....',
+                'timestamp': '10-11-18 3:00pm'
+            }
+        ]
+    return render_template('explore.html', title='Explore', posts=fakeposts)
+
+
